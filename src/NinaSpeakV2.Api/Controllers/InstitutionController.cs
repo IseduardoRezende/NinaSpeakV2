47cadc401acd89a95032c51d03ec27859eb8b7cc
Eduardo Rezende <dudumoises2005@gmail.com>
2024-07-05 16:18:12 -0300

    Atualizando Controller de Institution
    Sobrescrevendo Edit e alterando Details para manipular propriedade Members, tanto para visualização e configuração

--- src/NinaSpeakV2.Api/Controllers/InstitutionController.cs
+++ src/NinaSpeakV2.Api/Controllers/InstitutionController.cs
@@ -6,7 +6,6 @@ using NinaSpeakV2.Domain.Entities;
 using NinaSpeakV2.Domain.Services.IServices;
 using NinaSpeakV2.Domain.Validators;
 using NinaSpeakV2.Domain.ViewModels.Institutions;
-using NinaSpeakV2.Domain.ViewModels.Users;
 
 namespace NinaSpeakV2.Api.Controllers
 {
@@ -35,7 +34,7 @@ namespace NinaSpeakV2.Api.Controllers
                 return NotFound();
 
             var members = await _userInstitutionService.GetMembersByInstitutionFkAsync((long)institution.Id!);
-            institution.Members = members.Select(ui => new ReadUserViewModel { Email = ui.UserEmail });
+            institution.Members = members.OrderBy(ui => ui.UserEmail);
             
             return View(institution);
         }
@@ -50,6 +49,21 @@ namespace NinaSpeakV2.Api.Controllers
 
             createModel.UserFk = User.GetCurrentUserId();
             return await base.Create(createModel);
+        }
+
+        public override async Task<IActionResult> Edit(long? id)
+        {
+            var institution = await _readonlyService.GetByIdAsync(id ?? 0);
+
+            if (institution is null)
+                return NotFound();
+
+            var members = await _userInstitutionService.GetMembersByInstitutionFkAsync((long)institution.Id!);
+            
+            var updateModel = _mapper.Map<UpdateInstitutionViewModel>(institution);           
+            updateModel.Members = members.OrderBy(ui => ui.UserEmail);
+
+            return View(updateModel);
         }
     }
 }
