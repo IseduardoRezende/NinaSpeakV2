67ebfbbe5e680b771825c689321077aded028ef3
Eduardo Rezende <dudumoises2005@gmail.com>
2024-08-05 23:19:13 -0300

    Alterando Layout de View e Adição de Validação
    Alterando Layout de Detalhes de Usuario para ser usado como tela de 'Conta' e Adição de validação para apenas o dono da Conta poder se alterar

--- src/NinaSpeakV2.Api/Controllers/UserController.cs
+++ src/NinaSpeakV2.Api/Controllers/UserController.cs
@@ -1,13 +1,23 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NinaSpeakV2.Api.RequestValidators;
 using NinaSpeakV2.Data.Entities;
 using NinaSpeakV2.Domain.Services.IServices;
 using NinaSpeakV2.Domain.ViewModels.Users;
 
 namespace NinaSpeakV2.Api.Controllers
-{
+{    
     public class UserController : BaseController<User, CreateUserViewModel, UpdateUserViewModel, ReadUserViewModel>
     {
         public UserController(IUserService userService, IMapper mapper) : base(userService, mapper)
-        { }       
+        { }
+
+        public override async Task<IActionResult> Details(long? id)
+        {
+            if (!UserRequestValidator.IsHimself(id, User))
+                return Forbid();
+
+            return await base.Details(id);
+        }
     }
 }
