a32134add1bd05eb5f5ce1d44b925bea2563db52
Eduardo Rezende <dudumoises2005@gmail.com>
2024-08-07 16:46:08 -0300

    Organizando Constantes de Erros
    Organizando constantes de erros para ser mais fácil identificar e adicionar

--- NinaSpeakV2.Domain/Models/BaseError.cs
+++ NinaSpeakV2.Domain/Models/BaseError.cs
@@ -2,36 +2,43 @@
 {
     public class BaseError
     {
+        public const string NullObject = "Object Value is null.";
         public const string InternalProcessError = "An Internal Processing Error has Occurred.";
         public const string NoChangesDetected = "No changes was detected.";
         public const string InexistentObject = "Inexistent Object found.";
-        public const string NullObject = "Object Value is null.";
         public const string InvalidFilters = "Invalid Filters.";
         public const string InvalidIncludes = "Invalid Includes.";
         public const string InvalidValue = "Invalid Value.";
         public const string InvalidValues = "Invalid Values.";
 
+        public const string NameAlreadyExist = "Name already exist.";
+
         public const string UserNotFound = "User not found.";
         public const string UserNotAuthenticated = "User not authenticated.";
         public const string UserInstitutionNotFound = "User Institution not found.";
+        public const string UserInstitutionAlreadyExist = "Member already exists in the Institution.";
+
         public const string InstitutionNotFound = "Institution not found.";
+        
         public const string ChatBotNotFound = "ChatBot not found.";
         public const string ChatBotConversationNotFound = "ChatBot Conversation not found.";
         public const string ChatBotGenreNotFound = "ChatBot Genre not found.";
-        public const string PasswordNotMatch = "Password and Confirm Password don't match.";
+        
         public const string InvalidPassword = "Invalid Password.";
+        public const string PasswordNotMatch = "Password and Confirm Password don't match.";
+        
         public const string InvalidDescription = "Invalid Description.";
         public const string InvalidEmail = "Invalid Email.";
         public const string InvalidCode = "Invalid Institution Code.";
         public const string InvalidName = "Invalid Name.";
+        public const string InvalidImage = "Invalid Image.";        
         public const string InvalidMessage = "Invalid Message.";
         public const string InvalidResponse = "Invalid Response.";
-        public const string InvalidImage = "Invalid Image.";
+        
         public const string EmailAlreadyExist = "Email already exist.";
-        public const string NameAlreadyExist = "Name already exist.";
-        public const string UserInstitutionAlreadyExist = "Member already exists in the Institution.";
+        public const string EmailNotMatch = "New Email and Confirm New Email don't match.";
 
-        public const string UserInstitutionsErrorSpan = "An Institution is required to Create a ChatBot";
+        public const string UserInstitutionsErrorSpan = "An Institution is required to Create a ChatBot";        
         public const string ChatBotGenresErrorSpan = "An ChatBot Genre is required to Create a ChatBot";
         public const string ChatBotsErrorSpan = "An ChatBot is required to Create a Conversation";
 
