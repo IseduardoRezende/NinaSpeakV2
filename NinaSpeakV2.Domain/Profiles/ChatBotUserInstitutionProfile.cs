9eaa70dc5041fb6b643b30f93b12bf61e7b76b91
Eduardo Rezende <dudumoises2005@gmail.com>
2024-08-25 15:00:40 -0300

    Adição de Profile para ChatBotUserInstitution
    Adição de Profile para ChatBotUserInstitution poder ser mapeado pelo AutoMapper

---
+++ NinaSpeakV2.Domain/Profiles/ChatBotUserInstitutionProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using NinaSpeakV2.Data.Entities;
+using NinaSpeakV2.Domain.ViewModels.ChatBotUsersInstitutions;
+
+namespace NinaSpeakV2.Domain.Profiles
+{
+    public class ChatBotUserInstitutionProfile : Profile
+    {
+        public ChatBotUserInstitutionProfile()
+        {
+            CreateMap<CreateChatBotUserInstitutionViewModel, ChatBotUserInstitution>();
+            CreateMap<UpdateChatBotUserInstitutionViewModel, ChatBotUserInstitution>();
+            CreateMap<ReadChatBotUserInstitutionViewModel, UpdateChatBotUserInstitutionViewModel>();
+            CreateMap<ChatBotUserInstitution, ReadChatBotUserInstitutionViewModel>();
+        }
+    }
+}
