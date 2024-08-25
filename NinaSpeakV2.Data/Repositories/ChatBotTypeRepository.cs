2c72d90adf1290e97c393b711f3050ac88ebe21c
Eduardo Rezende <dudumoises2005@gmail.com>
2024-08-25 14:54:42 -0300

    Adição de ChatBotType em Repository
    Adição de nova entidade 'ChatBotType' na camada de Repository

---
+++ NinaSpeakV2.Data/Repositories/ChatBotTypeRepository.cs
@@ -0,0 +1,10 @@
+﻿using NinaSpeakV2.Data.Entities;
+using NinaSpeakV2.Data.Repositories.IRepositories;
+
+namespace NinaSpeakV2.Data.Repositories
+{
+    public class ChatBotTypeRepository : BaseReadonlyRepository<ChatBotType>, IChatBotTypeRepository
+    {
+        public ChatBotTypeRepository(NinaSpeakContext context) : base(context) { }
+    }
+}
---
+++ NinaSpeakV2.Data/Repositories/IRepositories/IChatBotTypeRepository.cs
@@ -0,0 +1,8 @@
+﻿using NinaSpeakV2.Data.Entities;
+
+namespace NinaSpeakV2.Data.Repositories.IRepositories
+{
+    public interface IChatBotTypeRepository : IBaseReadonlyRepository<ChatBotType>
+    {
+    }
+}
