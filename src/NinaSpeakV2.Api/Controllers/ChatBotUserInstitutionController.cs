201f892e85b58dc6aa84717a1d12c964b887e3d5
Eduardo Rezende <dudumoises2005@gmail.com>
2024-08-25 15:03:28 -0300

    Adição de Controller para ChatBotUserInstitution
    Adicionando camada Controller para nova entidade ChatBotUserInstitution

---
+++ src/NinaSpeakV2.Api/Controllers/ChatBotUserInstitutionController.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using NinaSpeakV2.Data.Entities;
+using NinaSpeakV2.Domain.Services.IServices;
+using NinaSpeakV2.Domain.ViewModels.ChatBotUsersInstitutions;
+
+namespace NinaSpeakV2.Api.Controllers
+{
+    public class ChatBotUserInstitutionController :
+                 BaseController<ChatBotUserInstitution, CreateChatBotUserInstitutionViewModel, UpdateChatBotUserInstitutionViewModel, ReadChatBotUserInstitutionViewModel>
+    {
+        public ChatBotUserInstitutionController(IChatBotUserInstitutionService chatBotUserInstitutionService, IMapper mapper) 
+            : base(chatBotUserInstitutionService, mapper)
+        { }
+    }
+}
