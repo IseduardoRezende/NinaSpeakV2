fe465b9fb9c1152d4a4f1f6e2d59290358628bb2
Eduardo Rezende <dudumoises2005@gmail.com>
2024-08-25 14:57:35 -0300

    Adição de Profile para ChatBotType
    Adição de Profile para ChatBotType poder ser mapeado pelo AutoMapper

---
+++ NinaSpeakV2.Domain/Profiles/ChatBotTypeProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using NinaSpeakV2.Data.Entities;
+using NinaSpeakV2.Domain.ViewModels.ChatBotTypes;
+
+namespace NinaSpeakV2.Domain.Profiles
+{
+    public class ChatBotTypeProfile : Profile
+    {
+        public ChatBotTypeProfile()
+        {
+            CreateMap<ChatBotType, ReadChatBotTypeViewModel>();
+        }
+    }
+}
