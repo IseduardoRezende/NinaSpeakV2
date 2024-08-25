e29e1eaef13338b4616d2f2473257ce6b2d80da8
Eduardo Rezende <dudumoises2005@gmail.com>
2024-08-25 15:09:27 -0300

    Atualizando Controller de ChatBot
    Atualizando Endpoint de Create em ChatBotController para se adequar a sua View e para armazenar corretamente os novos atributos

--- src/NinaSpeakV2.Api/Controllers/ChatBotController.cs
+++ src/NinaSpeakV2.Api/Controllers/ChatBotController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using NinaSpeakV2.Api.Extensions;
 using NinaSpeakV2.Api.Utils;
 using NinaSpeakV2.Data.Entities;
+using NinaSpeakV2.Domain.Extensions;
+using NinaSpeakV2.Domain.Models;
 using NinaSpeakV2.Domain.Services.IServices;
 using NinaSpeakV2.Domain.ViewModels.ChatBots;
 
@@ -11,15 +13,15 @@ namespace NinaSpeakV2.Api.Controllers
     public class ChatBotController
         : BaseController<ChatBot, CreateChatBotViewModel, UpdateChatBotViewModel, ReadChatBotViewModel>
     {
+        private readonly IChatBotTypeService _chatBotTypeService;
         private readonly IChatBotGenreService _chatBotGenreService;
         private readonly IUserInstitutionService _userInstitutionService;
 
-        //Validate ViewData ViewDataChatBotGenres and ViewDataUserInstitutions on HttpPost Actions to Views
-
-        public ChatBotController(IChatBotService chatBotService, IChatBotGenreService chatBotGenreService,
+        public ChatBotController(IChatBotService chatBotService, IChatBotTypeService chatBotTypeService, IChatBotGenreService chatBotGenreService,
                                  IUserInstitutionService userInstitutionService, IMapper mapper) 
             : base(chatBotService, mapper)
         {
+            _chatBotTypeService = chatBotTypeService;
             _chatBotGenreService = chatBotGenreService;
             _userInstitutionService = userInstitutionService;
         }
@@ -31,11 +33,31 @@ namespace NinaSpeakV2.Api.Controllers
 
         public override async Task<IActionResult> Create()
         {
+            if (ViewData.TryGetValues(Constant.ViewDataBaseErrors, out object? values))
+            {
+                ViewData.SetBaseErrors((values as IEnumerable<BaseError>)!);
+            }
+
             var userId = User.GetCurrentUserId();
 
+            ViewData[Constant.ViewDataChatBotTypes] = await _chatBotTypeService.GetAsync();
             ViewData[Constant.ViewDataChatBotGenres] = await _chatBotGenreService.GetAsync();
             ViewData[Constant.ViewDataUserInstitutions] = await _userInstitutionService.GetByOwnerAsync(userId);
             return View();
+        }
+
+        public override async Task<IActionResult> Create(CreateChatBotViewModel createModel)
+        {
+            var value = await _baseService.CreateAsync(createModel);
+
+            if (value.HasErrors())
+            {
+                ViewData.SetBaseErrors(value.BaseErrors!);
+                ViewData.TemporarilyStore();
+                return RedirectToAction("Create");
+            }
+
+            return RedirectToAction("Index", "Home");
         }
 
         public override async Task<IActionResult> Edit(long? id)
