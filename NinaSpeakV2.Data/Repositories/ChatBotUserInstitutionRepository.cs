336efa86f4cb3f5bcb706cca9a82c542cdae4873
Eduardo Rezende <dudumoises2005@gmail.com>
2024-08-25 14:58:33 -0300

    Adição de ChatBotUserInstitution em Repository
    Adição de nova entidade 'ChatBotUserInstitution' na camada de Repository

---
+++ NinaSpeakV2.Data/Repositories/ChatBotUserInstitutionRepository.cs
@@ -0,0 +1,10 @@
+﻿using NinaSpeakV2.Data.Entities;
+using NinaSpeakV2.Data.Repositories.IRepositories;
+
+namespace NinaSpeakV2.Data.Repositories
+{
+    public class ChatBotUserInstitutionRepository : BaseRepository<ChatBotUserInstitution>, IChatBotUserInstitutionRepository
+    {
+        public ChatBotUserInstitutionRepository(NinaSpeakContext context) : base(context) {  }
+    }
+}
---
+++ NinaSpeakV2.Data/Repositories/IRepositories/IChatBotUserInstitutionRepository.cs
@@ -0,0 +1,8 @@
+﻿using NinaSpeakV2.Data.Entities;
+
+namespace NinaSpeakV2.Data.Repositories.IRepositories
+{
+    public interface IChatBotUserInstitutionRepository : IBaseRepository<ChatBotUserInstitution>
+    {
+    }
+}
