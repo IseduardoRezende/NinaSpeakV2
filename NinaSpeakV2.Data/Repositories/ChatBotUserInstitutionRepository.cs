7484850b9f91760ec108f08c9c07349f231318c2
Eduardo Rezende <dudumoises2005@gmail.com>
2024-08-29 17:57:25 -0300

    Adição de Método para buscar informações de ChatBots pelo Membro
    Adição de método em camada Repository que retorna informações de ChatBots a partir do Membro

--- NinaSpeakV2.Data/Repositories/ChatBotUserInstitutionRepository.cs
+++ NinaSpeakV2.Data/Repositories/ChatBotUserInstitutionRepository.cs
@@ -1,4 +1,5 @@
-﻿using NinaSpeakV2.Data.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using NinaSpeakV2.Data.Entities;
 using NinaSpeakV2.Data.Repositories.IRepositories;
 
 namespace NinaSpeakV2.Data.Repositories
@@ -6,5 +7,14 @@ namespace NinaSpeakV2.Data.Repositories
     public class ChatBotUserInstitutionRepository : BaseRepository<ChatBotUserInstitution>, IChatBotUserInstitutionRepository
     {
         public ChatBotUserInstitutionRepository(NinaSpeakContext context) : base(context) {  }
+
+        public async Task<IEnumerable<ChatBotUserInstitution>> GetByUserIdAsync(long userId)
+        {
+            return await Task.FromResult(Entity
+                                        .Include(cbui => cbui.UserInstitution)
+                                        .ThenInclude(ui => ui.Institution)
+                                        .Include(cbui => cbui.ChatBot)
+                                        .Where(cbui => cbui.UserInstitution.UserFk == userId));
+        }
     }
 }
--- NinaSpeakV2.Data/Repositories/IRepositories/IChatBotUserInstitutionRepository.cs
+++ NinaSpeakV2.Data/Repositories/IRepositories/IChatBotUserInstitutionRepository.cs
@@ -4,5 +4,6 @@ namespace NinaSpeakV2.Data.Repositories.IRepositories
 {
     public interface IChatBotUserInstitutionRepository : IBaseRepository<ChatBotUserInstitution>
     {
+        Task<IEnumerable<ChatBotUserInstitution>> GetByUserIdAsync(long userId);
     }
 }
