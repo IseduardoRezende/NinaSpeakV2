be92b46143ef47d080f5bc04c147cdb8e04eaddc
Eduardo Rezende <dudumoises2005@gmail.com>
2024-08-31 23:09:48 -0300

    Atualizando métodos para obter Membros de ChatBot(s) em Repository
    Adição de método para obter membros pelo Id do ChatBot e refatorando método de obter os ChatBots que um Usuário é membro

--- NinaSpeakV2.Data/Repositories/ChatBotUserInstitutionRepository.cs
+++ NinaSpeakV2.Data/Repositories/ChatBotUserInstitutionRepository.cs
@@ -8,13 +8,31 @@ namespace NinaSpeakV2.Data.Repositories
     {
         public ChatBotUserInstitutionRepository(NinaSpeakContext context) : base(context) {  }
 
+        public async Task<IEnumerable<ChatBotUserInstitution>> GetMembersByChatBotFkAsync(long chatBotFk)
+        {
+            if (chatBotFk <= default(long))
+                return Enumerable.Empty<ChatBotUserInstitution>();
+
+            return await Entity
+                        .Include(cbui => cbui.ChatBot)
+                        .Include(cbui => cbui.UserInstitution)
+                            .ThenInclude(ui => ui.User)
+                        .Where(cbui => cbui.ChatBot.Id == chatBotFk)
+                        .OrderBy(cbui => cbui.UserInstitution.User.Email)
+                        .ToListAsync();
+        }
+
         public async Task<IEnumerable<ChatBotUserInstitution>> GetByUserIdAsync(long userId)
         {
-            return await Task.FromResult(Entity
-                                        .Include(cbui => cbui.UserInstitution)
-                                        .ThenInclude(ui => ui.Institution)
-                                        .Include(cbui => cbui.ChatBot)
-                                        .Where(cbui => cbui.UserInstitution.UserFk == userId));
+            if (userId <= default(long))
+                return Enumerable.Empty<ChatBotUserInstitution>();
+
+            return await Entity
+                        .Include(cbui => cbui.ChatBot)
+                        .Include(cbui => cbui.UserInstitution)
+                            .ThenInclude(ui => ui.Institution)
+                        .Where(cbui => cbui.UserInstitution.UserFk == userId)
+                        .ToListAsync();
         }
     }
 }
